b565419c85ddbae8079e9ad0ef9fd5a8b41bb392
edarzins <e.darzins@f5.com>
2023-04-05 10:02:06 -0600

    Sensor example code

---
+++ src/main/include/Robot.h
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <TimeOfFlight.h>
+#include <frc/AnalogInput.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  // Playing with Fusion ToF Sensor
+  frc::TimeOfFlight m_tofSensor{88};
+
+  // Adafruit/Sharp GP2Y0A21YK0F
+  frc::AnalogInput m_sensor1{0};
+
+  // Adafruit/Sharp GP2Y0D810Z0F
+  frc::AnalogInput m_sensor2{1};
+};
